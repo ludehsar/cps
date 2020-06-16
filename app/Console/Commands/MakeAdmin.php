58afcf2a44a5fc561d34f73921f92a9bff28fb04
Rashedul Alam <rashedulalam112112@gmail.com>
2020-06-16 21:36:46 +0600

    Implemented scripts for admins

---
+++ app/Console/Commands/MakeAdmin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class MakeAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:admin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Makes predefined users as admins.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        DB::unprepared(file_get_contents('admins.psql'));
+        $this->info('Admins have been populated successfully.');
+    }
+}
